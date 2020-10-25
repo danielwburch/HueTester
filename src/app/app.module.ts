6ae7927703a87168b594de8edcd56aea2f8be5e1
Daniel Burch <burchwdaniel@gmail.com>
2020-10-24 22:03:10 -0500

    Changed hue controller reference.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { HueControllerComponent } from './hue-controller/hue-controller.component';
+import { HueControllerComponent } from './components/hue-controller/hue-controller.component';
 
 @NgModule({
   declarations: [
